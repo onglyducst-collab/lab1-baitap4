1bdca2b91bc7a41f980349acd5e0c5d850d6edf9
onglyducst-collab <onglyducst.com>
2026-03-16 23:37:41 +0700

    lab1 baitap4

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+bool namnhuan(int a){
+    return (a % 4 == 0 && a % 100 != 0 || a % 400 == 0);
+}
+
+int main()
+{
+    int ngay, thang, nam;
+    cin >> ngay >> thang >> nam;
+    int songay = 0;
+    if (thang == 2){
+        if (namnhuan(nam)){
+            songay = 29;
+        }
+        else{
+            songay = 28;
+        }
+    }
+    else if (thang == 4 || thang == 6 || thang == 9 || thang == 11){
+        songay = 30;
+    }
+    else if (thang >= 1 && thang <= 12){
+        songay = 31;
+    }
+    else{
+        cout<<"Ngay khong hop le";
+        return 0;
+    }
+    if (ngay < 1 || ngay > songay){
+        cout<<"Ngay khong hop le";
+        return 0;
+    }
+    ngay++;
+    if (ngay > songay){
+        ngay = 1;
+        thang++;
+        if (thang > 12){
+            thang = 1;
+            nam++;
+        }
+    }
+    cout << ngay << "/" << thang << "/" << nam;
+    return 0;
+}
